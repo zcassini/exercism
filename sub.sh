2288523f66a4f2b43e8f0019310a1ccb8c6497c4
Zac Cassini <zcassini@gmail.com>
2023-12-16 10:47:18 -0500

    add a script to submit exercises more easily

---
+++ sub.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Save the current directory
+CURRENT_DIR=$(pwd)
+
+# Function to submit exercise
+submit_exercise() {
+    # Change to the kata directory
+    cd "$CURRENT_DIR/$1" || exit
+
+    # Submit the exercise
+    exercism submit
+
+    # Return to the original directory
+    cd "$CURRENT_DIR" || exit
+}
+
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 path/to/kata"
+    exit 1
+fi
+
+# Call the function with the kata path
+submit_exercise "$1"
+
