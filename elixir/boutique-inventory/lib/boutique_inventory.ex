d682ad46dea957605d20a0ab6e35a252c7cf2660
Zac Cassini <zcassini@gmail.com>
2023-12-16 10:47:52 -0500

    elixir boutique-inventory

---
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -0,0 +1,26 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory) do
+    Enum.sort(inventory, & &1[:price] <= &2[:price])
+  end
+
+  def with_missing_price(inventory) do
+    Enum.filter(inventory, &is_nil(&1[:price]))
+  end
+
+  def update_names(inventory, old_word, new_word) do
+    Enum.map(inventory, & put_in(&1[:name], String.replace(&1[:name], old_word, new_word)))
+  end
+
+  def increase_quantity(item, count) do
+    updated =
+    item[:quantity_by_size]
+    |> Enum.map(fn {key, quantity} -> {key, quantity + count} end)
+    |> Map.new
+
+    put_in(item[:quantity_by_size], updated)
+  end
+
+  def total_quantity(item) do
+    Enum.reduce(item[:quantity_by_size], 0, fn {_, quantity}, acc -> acc + quantity end)
+  end
+end
